1e1ed405066d7dddd78a712026464f716ab91fba
Kebba <kebba.sowe24@gmail.com>
2025-02-23 23:26:35 -0500

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.Scanner;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        Random rand = new Random();
+        double randomNum = rand.nextDouble(10) + 1;
+        double guess;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        do {
+            System.out.print("Guess a number between 1 and 10: ");
+            while (!scanner.hasNextDouble()) {
+                System.out.println("Invalid input. Please enter a number between 1 and 10.");
+                scanner.next();
+            }
+            guess = scanner.nextDouble();
+        } while (guess < 1 | guess > 10);
+
+        System.out.println("The guess was: ");
+        if (guess < randomNum) {
+            System.out.println("Your guess was too low");
+        } else if (guess > randomNum) {
+            System.out.println("Your guess was too high");
+        } else {
+            System.out.println("Your guess was correct");
         }
+
+        scanner.close();
     }
 }
+
